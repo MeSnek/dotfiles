8b43f11ac4ecb07d2cc496c844cb06ce2f603e17
sneku <sneakyfox61@gmail.com>
2023-07-24 17:19:51 -0400

    deez

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,7 +5,6 @@ dir="$HOME/.config/polybar"
 launch_bar() {
   killall polybar
   sleep 0.1
-  polybar -q -r secondary-top -c "$dir/config.ini" &
   polybar -q -r primary-top -c "$dir/config.ini" &
 }
 
